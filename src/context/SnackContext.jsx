c1413fedbdb63ba728a8d4a25034fa681b375129
meghamalviya5 <megha.malviya444@gmail.com>
2023-06-23 22:32:47 +0530

    changes --

--- src/context/SnackContext.jsx
+++ src/context/SnackContext.jsx
@@ -35,10 +35,63 @@ const SnackProvider = ({ children }) => {
       },
     });
   };
-  const sortByPrice = () => {};
-  const sortByName = () => {};
+  const sortByPrice = () => {
+    let sortedByPrice = [];
+    if (state.sortPriceAsc) {
+      sortedByPrice = state.filteredSnacks.sort((a, b) => a.price - b.price);
+    } else {
+      sortedByPrice = state.filteredSnacks.sort((a, b) => b.price - a.price);
+    }
+    dispatch({
+      type: "UPDATE_FILTERED_SNACKS",
+      payload: {
+        data: sortedByPrice,
+        sortKey: "sortPriceAsc",
+        sortValue: !state.sortPriceAsc,
+      },
+    });
+  };
+  const sortByName = () => {
+    let sortedByName = [];
+    if (state.sortNameAsc) {
+      sortedByName = state.filteredSnacks.sort(
+        (a, b) => a.product_name - b.product_name
+      );
+    } else {
+      sortedByName = state.filteredSnacks.sort(
+        (a, b) => b.product_name - a.product_name
+      );
+    }
+    dispatch({
+      type: "UPDATE_FILTERED_SNACKS",
+      payload: {
+        data: sortedByName,
+        sortKey: "sortNameAsc",
+        sortValue: !state.sortNameAsc,
+      },
+    });
+  };
   const sortByIngredients = () => {};
-  const sortByCalories = () => {};
+  const sortByCalories = () => {
+    let sortedByCalories = [];
+    if (state.sortCaloriesAsc) {
+      sortedByCalories = state.filteredSnacks.sort(
+        (a, b) => a.calories - b.calories
+      );
+    } else {
+      sortedByCalories = state.filteredSnacks.sort(
+        (a, b) => b.calories - a.calories
+      );
+    }
+    dispatch({
+      type: "UPDATE_FILTERED_SNACKS",
+      payload: {
+        data: sortedByCalories,
+        sortKey: "sortCaloriesAsc",
+        sortValue: !state.sortCaloriesAsc,
+      },
+    });
+  };
   const sortByWeight = () => {};
   console.log(state, "-======ncontext");
 
